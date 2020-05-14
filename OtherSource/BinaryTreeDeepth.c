5f73e44f5f1b521ea92a9a508f7275952f639c58
jasperye(叶宇轩) <jasperye@tencent.com>
2020-05-14 13:11:50 +0800

    [二叉树]，打印二叉树的右视图，使用Stack进行深度优先搜索

---
+++ OtherSource/BinaryTreeDeepth.c
@@ -0,0 +1,45 @@
+/**二叉树右视图深度优先搜索的解决方案
+ * 对比层序遍历使用队列，深度优先的办法使用了Stack，也是个有趣的思路
+ * 还是要注意不能死板，不是所有的二叉树题目都是递归
+ */
+
+//链接：https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/er-cha-shu-de-you-shi-tu-by-leetcode-solution/
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        unordered_map<int, int> rightmostValueAtDepth;
+        int max_depth = -1;
+
+        stack<TreeNode*> nodeStack;
+        stack<int> depthStack;
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.empty()) {
+            TreeNode* node = nodeStack.top();nodeStack.pop();
+            int depth = depthStack.top();depthStack.pop();
+
+            if (node != NULL) {
+                // 维护二叉树的最大深度
+                max_depth = max(max_depth, depth);
+
+                // 如果不存在对应深度的节点我们才插入
+                if (rightmostValueAtDepth.find(depth) == rightmostValueAtDepth.end()) {
+                    rightmostValueAtDepth[depth] =  node -> val;
+                }
+
+                nodeStack.push(node -> left);
+                nodeStack.push(node -> right);
+                depthStack.push(depth + 1);
+                depthStack.push(depth + 1);
+            }
+        }
+
+        vector<int> rightView;
+        for (int depth = 0; depth <= max_depth; ++depth) {
+            rightView.push_back(rightmostValueAtDepth[depth]);
+        }
+
+        return rightView;
+    }
+};
