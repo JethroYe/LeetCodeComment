bddd0d43a5c0f68d0ef2fe7ab39e1e8607d281a6
jasperye(叶宇轩) <jasperye@tencent.com>
2020-05-31 17:29:31 +0800

    在main中增加二叉树节点的定义

--- LeetCodeComment/main.swift
+++ LeetCodeComment/main.swift
@@ -8,10 +8,20 @@
 
 import Foundation
 
-class Solution {
-        
-}
+public class TreeNode {
+      public var val: Int
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init(_ val: Int) {
+          self.val = val
+          self.left = nil
+          self.right = nil
+      }
+  }
 
+class Solution {
+
+}
 func main() {    
     var s = Solution();
 }
